e182e0a05e87c991b772c49494e6fdca9f2a272d
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-19 23:49:03 +0530

    Added a debug log at the start

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -8,7 +8,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Ball is Rolling");
     }
 
     // Update is called once per frame
